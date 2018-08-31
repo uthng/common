387247319f5c48ffa868402a876933c106cd3f08
uthng <u.nguyenthanh@gmail.com>
2018-08-31 14:50:45 +0200

    - Add GetMapKeys without being sorted - Correction small bugs

--- utils/array_test.go
+++ utils/array_test.go
@@ -1,47 +1,47 @@
 package utils
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func TestStrInArrayStr(t *testing.T) {
-    var res bool
-    arrayStr := []interface{}{"toto", "titi", "tata", "tete"}
-    str1 := "titi"
-    str2 := "blabla"
-    testRes := false
+	var res bool
+	arrayStr := []interface{}{"toto", "titi", "tata", "tete"}
+	str1 := "titi"
+	str2 := "blabla"
+	testRes := false
 
-    fn := func (s1 interface{}, s2 interface{}) bool {
+	fn := func(s1 interface{}, s2 interface{}) bool {
 
-        if s1 == s2 {
-            return true
-        }
-        return false
-    }
+		if s1 == s2 {
+			return true
+		}
+		return false
+	}
 
-    res = common.IsElementInArray(str1, arrayStr, fn)
-    if res == true {
-        fmt.Printf("OK: \"%v\" is found in [%v]\n", str1, arrayStr)
-        testRes = true
-    } else {
-        fmt.Printf("ERR: \"%v\" is not found in [%v]\n", str1, arrayStr)
-    }
+	res = IsElementInArray(str1, arrayStr, fn)
+	if res == true {
+		fmt.Printf("OK: \"%v\" is found in [%v]\n", str1, arrayStr)
+		testRes = true
+	} else {
+		fmt.Printf("ERR: \"%v\" is not found in [%v]\n", str1, arrayStr)
+	}
 
-    if testRes == false {
-        t.Fail()
-    }
+	if testRes == false {
+		t.Fail()
+	}
 
-    res = common.IsElementInArray(str2, arrayStr, fn)
-    if res == true {
-        fmt.Printf("ERR: \"%v\" is found in [%v]\n", str2, arrayStr)
-    } else {
-        fmt.Printf("OK: \"%v\" is not found in [%v]\n", str2, arrayStr)
-        testRes = true
-    }
+	res = IsElementInArray(str2, arrayStr, fn)
+	if res == true {
+		fmt.Printf("ERR: \"%v\" is found in [%v]\n", str2, arrayStr)
+	} else {
+		fmt.Printf("OK: \"%v\" is not found in [%v]\n", str2, arrayStr)
+		testRes = true
+	}
 
-    if testRes == false {
-        t.Fail()
-    }
+	if testRes == false {
+		t.Fail()
+	}
 
 }
